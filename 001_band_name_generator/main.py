3c2a4d9f00280f131013dd5017715fde33c0cae2
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-11-06 17:47:41 -0300

    adding user friendly structure

--- 001_band_name_generator/main.py
+++ 001_band_name_generator/main.py
@@ -1,5 +1,5 @@
-print("Band Name Generator")
-print("-------------------")
+print("Welcome to the Band Name Generator")
+print("----------------------------------")
 
 city_name = input("What's the name of the city you grew up in? \n")
 pet_name = input("What's your pet's name? \n")
@@ -7,3 +7,5 @@ pet_name = input("What's your pet's name? \n")
 
 
 print(f"Your band name could be: {city_name} {pet_name}")
+print('----------------------------------')
+print("------------Goodbye---------------")
