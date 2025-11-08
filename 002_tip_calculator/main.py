88990320bc8c32ab0fbaad274b836743681a94e3
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-11-08 11:14:58 -0300

    fixing float numbers adding round function

--- 002_tip_calculator/main.py
+++ 002_tip_calculator/main.py
@@ -4,9 +4,9 @@ total_bill = float(input("What was the total bill? $\t"))
 tip = int(input("How much tip would you like to give? 10%, 12%, or 15%?\t"))
 num_person = int(input("How many people to split the bill?\t"))
 
-tip_value = total_bill * (tip/100)
+tip_value = round(total_bill * (tip/100), 2)
 bill_and_tip = total_bill + tip_value
-result = bill_and_tip/num_person
+result = round(bill_and_tip/num_person, 2)
 
 print("----------Calculation----------")
 print(f"Total bill: ${total_bill}")
