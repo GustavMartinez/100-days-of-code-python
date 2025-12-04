775856224218dcd547f49084369814a5df6c4e64
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-12-04 19:03:08 -0300

    cipher logic updated

--- 008_caesar_cipher/main.py
+++ 008_caesar_cipher/main.py
@@ -1,39 +1,45 @@
-
-"""
-
-TODO-1: \nCreate a function called `encrypt()` that takes `original_text` and `shift_amount` as 2 inputs.
-
-TODO-2: \nInside the 'encrypt' function, shift each letter of the `original_text` forwards in the alphabet by the `shift_amount` and print the encrypted text.
-
-TODO-3: \nCall the `encrypt()` function and pass in the user inputs. You should be able to test the code and encrypt a message.
-
-TODO-4: \nWhat happens if you try to shift the letter 'z' forwards by 9? Can you fix the code?
-
-"""
-
-
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def caesar(direction, original_text, shift_amount):
+
+    def encrypt(original_text, shift_amount):
+
+        encrypted = ""
+
+        for letter in original_text:
+            position = alphabet.index(letter) + shift_amount
+            position %= len(alphabet)
+            encrypted += alphabet[position]
+
+        print(f"Here is the encoded result: {encrypted}")
+
+
+    def decrypt(original_text, shift_amount):
+        
+        decrypt = ""
+        
+        for i in original_text:
+            position = alphabet.index(i) - shift_amount
+            position %= len(alphabet)
+            decrypt = decrypt + alphabet[position]
+        print(decrypt)
+
+
+    if direction == "encode":
+        encrypt(original_text, shift_amount)
+    
+    elif direction == "decode":
+        decrypt(original_text, shift_amount)
+
+    else:
+        print("No funciono")
+
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n").lower()
 text = input("Type your message: \n").lower()
 shift = int(input("Type the shift number:\n"))
 
-
-def encrypt(original_text, shift_amount):
-
-    encrypted = ""
-
-    for letter in original_text:
-        position = alphabet.index(letter) + shift_amount
-        position %= len(alphabet)
-        encrypted += alphabet[position]
-
-    print(f"Here is the encoded result: {encrypted}")
-
-
-
-encrypt(original_text=text, shift_amount=shift)
-
-
+caesar(direction, text, shift)
 
