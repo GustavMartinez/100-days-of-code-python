82b5774904c4fbb05612ff426fa9961252e1d6fb
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2026-01-15 03:41:38 -0300

    inicio del proyecto

---
+++ 012_number_guessing_game/main.py
@@ -0,0 +1,9 @@
+print()
+print("************************************")
+print("Welcome to the Number Guessing Game!")
+print("************************************")
+print()
+print("I'm thinking of a number between 1 and 100.")
+
+
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
