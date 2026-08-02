72fabc6b98a2f9b56d73b7facdec59445a8a6615
gust_dev <gustavo.a.t.m.12345@hotmail.com>
2026-08-01 21:46:20 -0300

    adicion de fomatacion

--- 001_band_name_generator/main.py
+++ 001_band_name_generator/main.py
@@ -1,11 +1,12 @@
-print("Welcome to the Band Name Generator")
-print("----------------------------------")
+print()
+print("----Welcome to the Band Name Generator-----")
+print("-------------------------------------------")
 
-city_name = input("What's the name of the city you grew up in? \n")
-pet_name = input("What's your pet's name? \n")
+city_name = input("What's the name of the city you grew up in? \n\t").capitalize()
+pet_name = input("What's your pet's name? \n\t").capitalize()
 
 
 
 print(f"Your band name could be: {city_name} {pet_name}")
-print('----------------------------------')
-print("------------Goodbye---------------")
+print('-------------------------------------------')
+print("----------------Goodbye--------------------")
